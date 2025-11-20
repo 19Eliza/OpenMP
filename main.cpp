c846944c53216d66dd0f0a85247df38aa443fed1
root <root@DESKTOP-SNKQT9A.localdomain>
2025-11-21 00:35:25 +0700

    Add result

--- main.cpp
+++ main.cpp
@@ -16,11 +16,13 @@ struct Ellipsoid {
     double a, b, c;  // полуоси
 };
 
-using tripleEllipsoid = std::array<Ellipsoid, 3>;
+using tripleEllipsoid = std::array<Ellipsoid, 3>; /// {E1,E2,E3}, brain(E1), skull(E2), skin(E3)
 
 // Тестовые случаи
 struct TestCase{
-    std::vector<tripleEllipsoid> testEllipsoid;// множество тестовых эллипсоидов
+    tripleEllipsoid test1{ Ellipsoid{30,40,50}, Ellipsoid{35,45,55}, Ellipsoid{40,50,60} };
+    tripleEllipsoid test2{Ellipsoid {30.0, 40.0, 50.0}, Ellipsoid {30.5, 40.5, 50.5}, Ellipsoid {31.0, 41.0, 51.0} };
+    std::vector<tripleEllipsoid> testEllipsoid{test1,test2};// множество тестовых эллипсоидов
     int countPointX = 32;
     int countPointY = 32;
     int countPointZ = 32;
@@ -132,9 +134,9 @@ void writeVoxelLine(ofstream& fout,
 int main()
 {
 
-    Ellipsoid E1{30, 40, 50};   // brain
-    Ellipsoid E2{35, 45, 55};   // skull
-    Ellipsoid E3{40, 50, 60};   // skin 
+    Ellipsoid E1{30.0, 40.0, 50.0}; // brain
+    Ellipsoid E2{30.5, 40.5, 50.5}; // skull
+    Ellipsoid E3{31.0, 41.0, 51.0}; // skin
 
     // ----------------------------------------------
     // Минимальный осевой параллелепипед
